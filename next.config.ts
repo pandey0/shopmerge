f4559cb2d329121ba92ef7c39a0c3ab5f4a1c6f5
pandey0 <arpit242002@gmail.com>
2026-01-29 20:35:49 +0530

    netlify

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  reactStrictMode: true,
+  // Any other Next.js configuration options
+};
 export default nextConfig;
